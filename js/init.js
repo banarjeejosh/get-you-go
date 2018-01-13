66d7f659ca0995deb5eb3f5c0136a2f79454020b
Joshio Banarjee <banarjeejosh@gmail.com>
2018-01-13 23:30:12 +0600

    First update of get you go html

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $(".dropdown-content").dropdown();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
